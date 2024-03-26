b2e325d71c2207a7fef607677ddf49b91e2714c2
Vadim Shender <vadim@shender.org>
2024-03-26 23:25:07 +0000

    Chapter 11: High-Level Languages

---
+++ 11.high-level-languages/hello-world.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/* PURPOSE: This program is mean to show a basic C program.  All it
+ * does is print "Hello World!" to the screen and exit.
+ */
+
+/* Main program. */
+int main(int argc, char **argv)
+{
+  /* Print out string to standard output. */
+  puts("Hello World!");
+
+  /* Exit with status 0. */
+  return 0;
+}
